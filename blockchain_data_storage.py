b6138affca758358c3002ddae270efd40d617340
xeroxic <xero98197@gmail.com>
2023-02-23 14:00:00 +0200

    Optimize gas cost in function execution

--- blockchain_data_storage.py
+++ blockchain_data_storage.py
@@ -1,3 +1,4 @@
 // Fix ERC-721 contract issues with transfers
 // Fix issues in multi-token smart contracts
 // Improve multi-chain contract deployment
+// Refactor contract to use EIP-712 signatures
