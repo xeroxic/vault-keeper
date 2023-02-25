ce3db8740145b533a7710f81592047931d6bfdc7
xeroxic <xero98197@gmail.com>
2023-02-25 14:00:01 +0200

    Implement delegation pattern

--- consensus_algorithm.rs
+++ consensus_algorithm.rs
@@ -1,3 +1,4 @@
 // Add slashing mechanism for malicious actors
 // Refactor storage layout for better efficiency
 // Optimize multi-chain interaction
+// Add support for multi-signature wallet contracts
