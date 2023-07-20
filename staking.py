e0ab9f2e621204adcd87bd65b1917edabaadc979
xeroxic <xero98197@gmail.com>
2023-07-20 14:00:00 +0200

    Optimize token minting flow

--- staking.py
+++ staking.py
@@ -1,3 +1,4 @@
 // Add support for decentralized insurance protocols
 // Improve token burning mechanism
 // Improve decentralized identity (DID) systems
+// Improve error handling in transaction flow
