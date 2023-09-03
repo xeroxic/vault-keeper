942d2d773c8a33c3748be2a8c05262b59faccdf1
xeroxic <xero98197@gmail.com>
2023-09-03 14:00:00 +0200

    Optimize token minting flow

--- bridge.go
+++ bridge.go
@@ -1,3 +1,4 @@
 // Optimize gas usage for contract deployment
 // Optimize gas usage for contract deployment
 // Improve contract execution performance
+// Add support for decentralized credit scoring
