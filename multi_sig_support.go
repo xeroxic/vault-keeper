a7baa5c11032b1849e0f2da625bcc6b8b8b548be
xeroxic <xero98197@gmail.com>
2023-09-25 14:00:00 +0200

    Add support for ERC-721A tokens

--- multi_sig_support.go
+++ multi_sig_support.go
@@ -2,3 +2,4 @@
 // Add support for batch transaction processing
 // Improve contract execution performance
 // Integrate decentralized anonymous identity systems
+// Improve security for token minting contracts
