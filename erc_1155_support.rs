33ce473df94f04a329a2c26836c07946a7099b84
xeroxic <xero98197@gmail.com>
2023-10-01 14:00:00 +0200

    Improve error handling in transaction flow

--- erc_1155_support.rs
+++ erc_1155_support.rs
@@ -1,3 +1,4 @@
 // Refactor contract to use EIP-712 signatures
 // Improve error handling in transaction flow
 // Integrate with Ethereum Layer-2 solutions
+// Add fallback mechanism in Solidity contract
