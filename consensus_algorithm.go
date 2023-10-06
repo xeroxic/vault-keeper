a7edde16a1b8f4df5cf0f2ba3a3838a626faa989
xeroxic <xero98197@gmail.com>
2023-10-06 14:00:00 +0200

    Reduce gas cost in minting logic

--- consensus_algorithm.go
+++ consensus_algorithm.go
@@ -3,3 +3,4 @@
 // Integrate zk-SNARKs for enhanced privacy
 // Improve gas estimation for transactions
 // Fix security vulnerability in random number generation
+// Optimize oracle data fetching mechanism
