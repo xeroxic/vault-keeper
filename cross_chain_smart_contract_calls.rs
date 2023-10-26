f022ed1d937bccaefcb6b25c88b7d06c1086aa66
xeroxic <xero98197@gmail.com>
2023-10-26 14:00:01 +0200

    Add native support for staking rewards

--- cross_chain_smart_contract_calls.rs
+++ cross_chain_smart_contract_calls.rs
@@ -1,3 +1,4 @@
 // Add governance proposal expiration feature
 // Fix security vulnerability in random number generation
 // Add governance proposal expiration feature
+// Add support for cross-chain bridges
