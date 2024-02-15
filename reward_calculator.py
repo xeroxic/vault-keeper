bb4bf6507e583af3282443af45d0e510c87f307f
xeroxic <xero98197@gmail.com>
2024-02-15 14:00:00 +0200

    Add support for ERC-1155 tokens

--- reward_calculator.py
+++ reward_calculator.py
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Implement reward distribution feature
 // Improve decentralized exchange (DEX) liquidity
+// Fix issues with token transfer confirmations
