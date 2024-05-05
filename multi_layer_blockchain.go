43e31c5e319bee935c2bd0197b7dad21fb8fdeec
xeroxic <xero98197@gmail.com>
2024-05-05 14:00:01 +0200

    Fix transaction replay issue

--- multi_layer_blockchain.go
+++ multi_layer_blockchain.go
@@ -1,3 +1,4 @@
 // Add support for multiple consensus mechanisms
 // Fix bugs in decentralized exchange routing
 // Add support for decentralized content distribution
+// Optimize storage for off-chain data
