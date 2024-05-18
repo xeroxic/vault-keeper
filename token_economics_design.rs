74f16de50aa35f5d51ec01392eda8a64f836dc22
xeroxic <xero98197@gmail.com>
2024-05-18 14:00:01 +0200

    Refactor governance token logic

--- token_economics_design.rs
+++ token_economics_design.rs
@@ -1,3 +1,4 @@
 // Improve API rate limiting
 // Improve transaction batching efficiency
 // Fix interoperability issues with other blockchains
+// Fix interoperability issues with other blockchains
