e49036d6f701853922ecdc5a150e0478ccc48104
xeroxic <xero98197@gmail.com>
2024-05-30 14:00:01 +0200

    Fix interoperability issues with other blockchains

--- staking_rewards.go
+++ staking_rewards.go
@@ -1,3 +1,4 @@
 // Fix issues with cross-chain transaction finality
 // Add new feature: on-chain governance
 // Fix issues in token transfer logic
+// Integrate with decentralized identity providers
