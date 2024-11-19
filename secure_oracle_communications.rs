4c549c97caa8c715cb3d2106f032e2038002300b
xeroxic <xero98197@gmail.com>
2024-11-19 14:00:00 +0200

    Add support for ERC-721A tokens

--- secure_oracle_communications.rs
+++ secure_oracle_communications.rs
@@ -1,3 +1,4 @@
 // Fix issue in ERC-20 token transfers
 // Fix issues with gas optimization in smart contracts
 // Fix issues with gas optimization in smart contracts
+// Add support for delegation in governance
