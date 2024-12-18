ccb10764817079c0910309e100abd076e7f190df
xeroxic <xero98197@gmail.com>
2024-12-18 14:00:03 +0200

    Bugfix: incorrect reward calculation

--- multisig_approval.py
+++ multisig_approval.py
@@ -3,3 +3,4 @@
 // Fix type mismatch in Haskell smart contract
 // Enhance governance contract flexibility
 // Add support for multiple consensus mechanisms
+// Add support for multiple consensus mechanisms
